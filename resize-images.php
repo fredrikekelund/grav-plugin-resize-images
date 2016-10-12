b8d42df2f4eaa30ac4151f5905657163515e229c
Fredrik Ekelund <fredrik@fredrik.computer>
2016-10-12 22:48:01 +0200

    Just some code cleanup
    Split ResizeImagesPlugin#resizeImages method into multiple
    methods. Separates responsibilities more clearly.

--- resize-images.php
+++ resize-images.php
@@ -32,6 +32,85 @@ class ResizeImagesPlugin extends Plugin
     }
 
     /**
+     * Determine which adapter is preferred and whether or not it's available
+     * @return string - Either 'imagick' or 'gd'
+     */
+    protected function getImageAdapter()
+    {
+        $imagick_exists = class_exists('\Imagick');
+        $gd_exists = extension_loaded('gd');
+
+        $use_imagick = $imagick_exists ? $this->adapter == 'imagick' : false;
+        $use_gd = $gd_exists ? $this->adapter == 'gd' : false;
+
+        return $use_imagick ? 'imagick' : $use_gd ? 'gd' : false;
+    }
+
+    /**
+     * Resize an image using Imagick
+     * @param  string $source         - Source image path
+     * @param  string $target         - Target image path
+     * @param  float $width           - Target width
+     * @param  float $height          - Target height
+     * @param  int [$quality]         - Compression quality for target image
+     * @return bool                   - Returns true on success, otherwise false
+     */
+    protected function resizeImageImagick($source, $target, $width, $height, $quality)
+    {
+        $image = new \Imagick($source);
+        $image->resizeImage($width, $height, \Imagick::FILTER_LANCZOS, 1);
+        $image->setImageCompressionQuality($quality);
+        $result = $image->writeImage($target);
+        $image->clear();
+        return (bool) $result;
+    }
+
+    /**
+     * Resize an image using GD
+     * @param  string $source         - Source image path
+     * @param  string $target         - Target image path
+     * @param  float $width           - Target width
+     * @param  float $height          - Target height
+     * @param  int [$quality]         - Compression quality for target image
+     * @param  float [$source_width]  - Width of source image. Only necessary if using GD, and will be calculated if not supplied
+     * @param  float [$source_height] - Height of source image. Only necessary if using GD, and will be calculated if not supplied
+     * @return bool                   - Returns true on success, otherwise false
+     */
+    protected function resizeImageGd($source, $target, $width, $height, $quality, $source_width, $source_height)
+    {
+        if (!$source_width || !$source_height) {
+            $size = getimagesize($source);
+            $source_width = $size[0];
+            $source_height = $size[1];
+        }
+
+        $target_info = pathinfo($target);
+        $dest_image = imagecreatetruecolor($width, $height);
+
+        if (preg_match('/jpe?g/', $target_info['extension'])) {
+            $source_image = imagecreatefromjpeg($source);
+            imagecopyresampled($dest_image, $source_image, 0, 0, 0, 0, $width, $height, $source_width, $source_height);
+
+            $result = imagejpeg($dest_image, $target, $quality);
+        } else if ($target_info['extension'] == 'png') {
+            $source_image = imagecreatefrompng($source);
+            $transparent = imagecolorallocatealpha($dest_image, 255, 255, 255, 127);
+
+            imagealphablending($dest_image, false);
+            imagesavealpha($dest_image, true);
+            imagefilledrectangle($dest_image, 0, 0, $width, $height, $transparent);
+            imagecopyresampled($dest_image, $source_image, 0, 0, 0, 0, $width, $height, $source_width, $source_height);
+
+            $result = imagepng($dest_image, $target, 9);
+        }
+
+        imagedestroy($source_image);
+        imagedestroy($dest_image);
+
+        return $result;
+    }
+
+    /**
      * Resizes an image using either Imagick or GD
      * @param  string $source         - Source image path
      * @param  string $target         - Target image path
@@ -44,57 +123,14 @@ class ResizeImagesPlugin extends Plugin
      */
     protected function resizeImage($source, $target, $width, $height, $quality, $source_width, $source_height)
     {
-        $imagick_exists = class_exists('\Imagick');
-        $gd_exists = extension_loaded('gd');
-        $use_imagick = $imagick_exists ? $this->adapter == 'imagick' : false;
-        $use_gd = $gd_exists ? $this->adapter == 'gd' : false;
+        $adapter = $this->getImageAdapter();
 
-        if ($use_imagick) {
-
-            $image = new \Imagick($source);
-            $image->resizeImage($width, $height, \Imagick::FILTER_LANCZOS, 1);
-            $image->setCompressionQuality($quality);
-            $result = $image->writeImage($target);
-            $image->clear();
-            return (bool) $result;
-
-        } else if ($use_gd) {
-
-            if (!$source_width || !$source_height) {
-                $size = getimagesize($source);
-                $source_width = $size[0];
-                $source_height = $size[1];
-            }
-
-            $target_info = pathinfo($target);
-            $dest_image = imagecreatetruecolor($width, $height);
-
-            if (preg_match('/jpe?g/', $target_info['extension'])) {
-                $source_image = imagecreatefromjpeg($source);
-                imagecopyresampled($dest_image, $source_image, 0, 0, 0, 0, $width, $height, $source_width, $source_height);
-
-                $result = imagejpeg($dest_image, $target, $quality);
-            } else if ($target_info['extension'] == 'png') {
-                $source_image = imagecreatefrompng($source);
-                $transparent = imagecolorallocatealpha($dest_image, 255, 255, 255, 127);
-
-                imagealphablending($dest_image, false);
-                imagesavealpha($dest_image, true);
-                imagefilledrectangle($dest_image, 0, 0, $width, $height, $transparent);
-                imagecopyresampled($dest_image, $source_image, 0, 0, 0, 0, $width, $height, $source_width, $source_height);
-
-                $result = imagepng($dest_image, $target, (int) ($quality / 100) * 9);
-            }
-
-            imagedestroy($source_image);
-            imagedestroy($dest_image);
-
-            return $result;
-
+        if ($adapter == 'imagick') {
+            return $this->resizeImageImagick($source, $target, $width, $height, $quality);
+        } else if ($adapter == 'gd') {
+            return $this->resizeImageGd($source, $target, $width, $height, $quality, $source_width, $source_height);
         } else {
-
             return false;
-
         }
     }
 
